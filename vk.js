909fd96cb4a9fb7415d416d063b16aa8afda2205
Vishal Prajapati <prajapativishalofficial@gmail.com>
2021-05-07 20:42:34 +0530

    Inprograce

---
+++ vk.js
@@ -0,0 +1,13 @@
+<script>
+var i = 0;
+var txt = 'We Are Here To Decorate Your Life';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+</script>
